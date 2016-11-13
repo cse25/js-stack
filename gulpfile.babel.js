168d96f247e23792c76fb1f62dd76e6cb01c5d5a
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-11-13 14:13:32 -0800

    modern javascript stack boilerplate

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+
+import webpack from 'webpack-stream';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
+import del from 'del';
+import webpackConfig from './webpack.config.babel';
+
+const paths = {
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
+  gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
+  libDir: 'lib',
+  distDir: 'dist',
+};
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+    paths.webpackFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()),
+);
+
+gulp.task('clean', () => del(paths.libDir));
+
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir)),
+);
+
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir)),
+);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('default', ['watch', 'main']);
